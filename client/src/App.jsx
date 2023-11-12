7de3a93d637c7db892b5b7056197faf42944cef8
Carlos Pacheco <carlospacheco.fx@gmail.com>
2023-11-12 18:06:46 -0400

    change

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,27 +2,14 @@ import { useEffect } from 'react';
 import { Navbar } from './components';
 
 function App() {
-<<<<<<< HEAD
   const getData = async (data) => {
     try {
       await fetch();
-=======
-  const getData = async () => {
-    const email = 'carlos@example.com';
-    try {
-      const reponse = await fetch(`http://localhost:8080/users`);
-      const json = await response.json();
-      console.log(json);
->>>>>>> features
     } catch (error) {
       console.error(error);
     }
   };
 
-<<<<<<< HEAD
-=======
-  useEffect(() => getData(), []);
->>>>>>> features
   return (
     <div>
       <Navbar />
