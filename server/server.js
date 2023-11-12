657eeaceee6c5d219370bd4ef90320aff99af5fa
Carlos Pacheco <carlospacheco.fx@gmail.com>
2023-11-12 16:06:15 -0400

    Modified

--- server/server.js
+++ server/server.js
@@ -4,6 +4,6 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from nodejs');
+  res.send('Hello from nodejssss');
 });
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
