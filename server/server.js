26c027a1b6e33adec24b0824422a8d8f7244cee7
Carlos Pacheco <carlospacheco.fx@gmail.com>
2023-11-12 16:50:49 -0400

    mod

--- server/server.js
+++ server/server.js
@@ -5,20 +5,17 @@ const PORT = process.env.PORT ?? 8000;
 const app = express();
 const pool = require('./db');
 
-/*
 app.get('/users', async (req, res) => {
   try {
+    const users = await pool.query('SELECT * from users');
+    res.json(users.rows[0]);
   } catch (error) {
     console.error(error);
   }
 });
-*/
+
 app.get('/', (req, res) => {
   res.send('Bienvenidoo');
 });
 
-app.get('/ping', async (req, res) => {
-  const result = await pool.query('SELECT NOW()');
-  return res.json(result.rows[0]);
-});
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
