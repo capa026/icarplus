35cb445f73cc843a7d30c59e7bafe514d4a13af2
Carlos Pacheco <carlospacheco.fx@gmail.com>
2023-11-15 21:46:31 -0400

    DELETE

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { createCopyPlugin } from 'vite-plugin-copy';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    createCopyPlugin({
-      patterns: [{ from: '_redirects', to: '_redirects' }],
-    }),
-  ],
+  plugins: [react()],
 });
