16aadb7f18fcce1198c75d939de8ab5436d43c8e
Carlos Pacheco <carlospacheco.fx@gmail.com>
2023-11-19 16:24:52 -0400

    Feed updated

--- client/src/pages/Feed.jsx
+++ client/src/pages/Feed.jsx
@@ -1,4 +1,10 @@
+import bg from '../assets/461185-superdeportivo-hibrido-mercedes-amg-podria-ser-v12.webp';
 const Feed = () => {
-  return <div>Feed</div>;
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>Pagina de inicio</h1>
+      <img src={bg} alt="" srcset="" style={{ width: '100%' }} />
+    </div>
+  );
 };
 export default Feed;
