4f68e32caa7b30e60c62766b49bf0d67b347693f
Carlos Pacheco <carlospacheco.fx@gmail.com>
2023-11-19 15:37:47 -0400

    navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,8 @@
 const Navbar = () => {
-  return <div className="navbar">Navbar</div>;
+  return (
+    <div className="navbar">
+      <h1>IcarPlus corp.</h1>
+    </div>
+  );
 };
 export default Navbar;
