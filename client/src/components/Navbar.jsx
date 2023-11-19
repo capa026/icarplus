76134e70e6a2f6c15806076d055a9a949ccac681
Carlos Pacheco <carlospacheco.fx@gmail.com>
2023-11-19 15:41:01 -0400

    NAVBAR

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,13 @@
+import { CarOutlined } from '@ant-design/icons';
+
 const Navbar = () => {
-  return <div className="navbar">Navbar</div>;
+  return (
+    <div className="navbar">
+      <h1>
+        <CarOutlined />
+        IcarPlus CORP.
+      </h1>
+    </div>
+  );
 };
 export default Navbar;
